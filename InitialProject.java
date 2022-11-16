38477253a873dd703a39552420f6dde93b35f6d1
Vinicius Rosa <viniciushrk@gmail.com>
2022-11-16 15:03:18 -0400

    feat: initialProject

---
+++ InitialProject.java
@@ -0,0 +1,8 @@
+import models.Cliente;
+
+public class InitialProject {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.CriarThread();
+    }
+}
